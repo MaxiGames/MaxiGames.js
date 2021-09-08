4f0cd11e6e7f1647bd2df7b45508506ce233f21a
nianny <xiaonianhe@hotmail.com>
2021-09-08 21:18:12 +0800

    weiord

--- index.ts
+++ index.ts
@@ -8,6 +8,16 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -16,23 +26,18 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
+
 	const command = client.commands.get(interaction.commandName);
 	if (!command) return;
 	try {
 		await command.execute(interaction);
 	} 
-    catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
+  catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
 
