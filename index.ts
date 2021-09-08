4389260a849bcfe5990535a630ef3173b5ffec3c
nianny <xiaonianhe@hotmail.com>
2021-09-08 16:51:20 +0800

    hm

--- index.ts
+++ index.ts
@@ -1,16 +1,40 @@
-const { Client, Intents } = require("discord.js");
+//literally all copy-pasted
+
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+
+
+client.once('ready', () => {
+    console.log("Ready!");
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+	try {
+		await command.execute(interaction);
+	} 
+    catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
-client.login("token");
+
+client.login(token);
+
