fb79b800e389108fbe855ae9b3b3266115e66300
Isaac <tux314159@gmail.com>
2021-09-10 02:42:43 +0800

    Even better pre-commit!

--- scripts/check-commands.js
+++ scripts/check-commands.js
@@ -1,40 +1,30 @@
 "use strict";
-exports.__esModule = true;
-var fs = require("fs");
-var path = require("path");
-var process_1 = require("process");
-var commands = [];
-// NOTE: The directory "commands" should contain subdirectories to organise js commands.
-var commandFiles = fs
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+
+const commands = [];
+
+const commandFiles = fs
   .readdirSync("./dist/src/commands")
-  .map(function (file) {
-    return path.join("./dist/src/commands", file);
-  })
-  .filter(function (file) {
-    return fs.lstatSync(file).isDirectory();
-  })
-  .map(function (dir) {
-    return [
-      dir,
-      fs.readdirSync(dir).filter(function (file) {
-        return file.endsWith(".js");
-      }),
-    ];
-  });
-for (
-  var _i = 0, commandFiles_1 = commandFiles;
-  _i < commandFiles_1.length;
-  _i++
-) {
-  var filecol = commandFiles_1[_i];
-  for (var _a = 0, _b = filecol[1]; _a < _b.length; _a++) {
-    var name_1 = _b[_a];
-    var command = require("../" + path.join(filecol[0], name_1)); // funny path-fu
+  .map((file) => path.join("./dist/src/commands", file))
+  .filter((file) => fs.lstatSync(file).isDirectory())
+  .map((dir) => [
+    dir,
+    fs.readdirSync(dir).filter((file) => file.endsWith(".js")),
+  ]);
+
+for (const filecol of commandFiles) {
+  for (const name of filecol[1]) {
+    const command = require(`../${path.join(filecol[0], name)}`); // funny path-fu
     try {
-      command["default"].data.toJSON();
-    } catch (_c) {
-      (0, process_1.exit)(1);
+      commands.push(command.default.data.toJSON());
+    } catch {
+      console.error(`Invalid command file: ${path.join(filecol[0], name)}`);
+      console.error(`Hint: delete it and its corresponding typescript source`);
+      console.error(`      file, or export placeholder data (e.g. an empty`);
+      console.error(`      execute function with a bare newSlashCommand).`);
+      process.exit(1);
     }
   }
 }
-(0, process_1.exit)(0);
