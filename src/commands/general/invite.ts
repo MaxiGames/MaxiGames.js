f0cf5c563b177f1926d102155ef6abc2887ec0e8
Isaac <tux314159@gmail.com>
2021-09-10 02:14:32 +0800

    Nicer presentation for invites

--- src/commands/general/invite.ts
+++ src/commands/general/invite.ts
@@ -8,9 +8,9 @@ const invite: MyCommand = {
     .setDescription("Invite maxigames to your server :D"),
   async execute(interaction) {
     const embed = new MessageEmbed()
-      .setTitle("Invite link for Maxigames :D")
+      .setTitle("Invite Maxigames :D")
       .setDescription(
-        "https://discord.com/api/oauth2/authorize?client_id=863419048041381920&permissions=399397481590&scope=bot%20applications.commands"
+        "[Click here!](https://discord.com/api/oauth2/authorize?client_id=863419048041381920&permissions=399397481590&scope=bot%20applications.commands)"
       )
       .setColor("#57F287");
     await interaction.reply({ embeds: [embed] });
