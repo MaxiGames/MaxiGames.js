80b708ca3f3b3e0d9fda668892ac76f4873156ee
nianny <xiaonianhe@hotmail.com>
2021-09-09 21:54:04 +0800

    me starting doing stuff

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,6 +4,7 @@ const ready: MyEvent = {
   name: "ready",
   once: true,
   execute(client) {
+    if (client.user == null) return;
     console.log(`Ready! Logged in as ${client.user.tag}`);
     // client.application.commands.set([])
     // client.guilds.cache.map(guild => guild.commands.set([]));
