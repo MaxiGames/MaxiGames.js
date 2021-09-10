edb2b78a72e4083e5b0df5caf003697c6cfac851
Isaac <tux314159@gmail.com>
2021-09-10 14:26:56 +0800

    scripts -> js for more speed, and scripts/ -> .scripts

---
+++ .scripts/deploy-commands.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const config = require("../dist/src/config.js");
+
+const commands = [];
+
+// NOTE: The directory "commands" should contain subdirectories to organise js commands.
+const commandFiles = fs
+  .readdirSync("./dist/src/commands")
+  .map((file) => path.join("./dist/src/commands", file))
+  .filter((file) => fs.lstatSync(file).isDirectory())
+  .map((dir) => [
+    dir,
+    fs.readdirSync(dir).filter((file) => file.endsWith(".js")),
+  ]);
+
+for (const filecol of commandFiles) {
+  for (const name of filecol[1]) {
+    const command = require(`../${path.join(filecol[0], name)}`); // funny path-fu
+    commands.push(command.default.data.toJSON());
+    console.log(`Registered ${name}`);
+  }
+}
+
+const rest = new REST({ version: "9" }).setToken(config.default.tokenId);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        config.default.clientId,
+        config.default.guildId
+      ),
+      {
+        body: commands,
+      }
+    );
+
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
