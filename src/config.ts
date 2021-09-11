d622c3084d8d23a99588dd49203308a3ab3e3605
wangy412 <wangyunze412@gmail.com>
2021-09-11 11:09:35 +0800

    Rename serviceAccountKey.json to be more descriptivev of prod vs dev

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 import configProd from "../config-prod.json";
 import configDev from "../config-dev.json";
-import serviceAccountKey from "../serviceAccountKey.json";
-import serviceAccountKey2 from "../serviceAccountKey2.json";
+import serviceAccountKeyProd from "../serviceAccountKey-prod.json";
+import serviceAccountKeyDev from "../serviceAccountKey-dev.json";
 
 export const config =
   process.env.NODE_ENV == "production" ? configProd : configDev;
 
 export const firebaseConfig =
-  process.env.NODE_ENV == "production" ? serviceAccountKey : serviceAccountKey2;
+  process.env.NODE_ENV == "production"
+    ? serviceAccountKeyProd
+    : serviceAccountKeyDev;
