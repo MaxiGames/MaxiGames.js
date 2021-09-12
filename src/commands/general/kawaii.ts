0bea45ef8e3889f2edc528a3b47581ed7a7d5755
neon_paradox <glennwy.sun@gmail.com>
2021-09-12 15:18:04 +0800

    Revert "owoify!!!"
    This reverts commit 7f4a5935ff6e60585d3e46b2205fb27872d67e31.

--- src/commands/general/kawaii.ts
+++ src/commands/general/kawaii.ts
@@ -1,29 +1,7 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import MGCommand from "../../types/command";
-const owo = require("owoify-js").default;
 
 const Discord = require("discord.js");
-
-function lessOwo(msg: string) {
-  let message_1 = msg;
-  let message_2 = msg.replace("ss", "s");
-  let message_2_storage = message_2;
-  while (message_1 != message_2) {
-    message_2_storage = message_2;
-    message_2 = message_2.replace("ss", "s");
-    message_1 = message_2_storage;
-  }
-
-  message_2 = message_2
-    .replace("sh", "s")
-    .replace("s", "sh")
-    .replace("nine", "9")
-    .replace("one", "1")
-    .replace("for", "4")
-    .replace("rr", "ww");
-  return message_2;
-}
-
 const kawaii: MGCommand = {
   data: new SlashCommandBuilder()
     .setName("kawaii")
@@ -33,21 +11,10 @@ const kawaii: MGCommand = {
         .setName("message")
         .setDescription("Your message to be kawaii-ified")
         .setRequired(true)
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("verycute")
-        .setDescription("Make Ur Message extra cute")
-        .setRequired(false)
     ),
   async execute(interaction) {
     const msg: string =
       interaction.options.getString("message") || "Invalid :(";
-    const cute: boolean = interaction.options.getBoolean("verycute") || false;
-    let res = "";
-    if (cute) res = owo(msg);
-    else res = lessOwo(msg);
-
     if (msg.length > 2000) {
       const Embed = new Discord.MessageEmbed()
         .setColor("#ff0000")
@@ -58,11 +25,26 @@ const kawaii: MGCommand = {
       });
       return;
     }
+    let message_1 = msg;
+    let message_2 = msg.replace("ss", "s");
+    let message_2_storage = message_2;
+    while (message_1 != message_2) {
+      message_2_storage = message_2;
+      message_2 = message_2.replace("ss", "s");
+      message_1 = message_2_storage;
+    }
 
+    message_2 = message_2
+      .replace("sh", "s")
+      .replace("s", "sh")
+      .replace("nine", "9")
+      .replace("one", "1")
+      .replace("for", "4")
+      .replace("rr", "ww");
     const Embed = new Discord.MessageEmbed()
       .setColor("#00ff00")
       .setTitle(`Your kawaii-ified message: `)
-      .setDescription(`${res}`);
+      .setDescription(`${message_2}`);
     await interaction.reply({
       embeds: [Embed],
     });
