d8b601235dc6cbf33b7deb2704f87f5f52e6be82
neon_paradox <glennwy.sun@gmail.com>
2021-09-13 19:45:45 +0800

    ship system based on ascii :O
     may or may not make sense :(

--- src/commands/general/ship.ts
+++ src/commands/general/ship.ts
@@ -1,64 +1,67 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { MGEmbed } from "../../lib/flavoured";
+import MGStatus from "../../lib/statuses";
 import MGCommand from "../../types/command";
 
 const Discord = require("discord.js");
 const ship: MGCommand = {
   data: new SlashCommandBuilder()
     .setName("ship")
-    .setDescription("Test your relationship :O [Gives a percentage based on how well two strings complement each other]")
-    .addIntegerOption((option) =>
+    .setDescription(
+      "Test your relationship :O [Gives a percentage based on how well two strings complement each other]"
+    )
+    .addStringOption((option) =>
       option
         .setName("object")
         .setDescription("First object (Any string)")
         .setRequired(true)
     )
-    .addIntegerOption((option) =>
+    .addStringOption((option) =>
       option
         .setName("object2")
-        .setDescription("Second object (if left blank, default set as your username)")
+        .setDescription(
+          "Second object (if left blank, default set as your username)"
+        )
         .setRequired(false)
     ),
   async execute(interaction) {
-    const sides: number = interaction.options.getInteger("sides") || 6;
-    const dice: number = interaction.options.getInteger("dice_count") || 1;
-    if (dice > 200) {
+    const firstname: string =
+      interaction.options.getString("object") || "maxigames";
+    const secondname: string =
+      interaction.options.getString("object2") || interaction.user.username;
+    if (firstname.length > 1000) {
       await interaction.reply({
-        content: "Maxigames doesn't own that many dice :(",
+        content: "Your first name was too long! Try something shorter!",
         ephemeral: true,
       });
       return;
     }
-    if (sides > 1000) {
+    if (secondname.length > 1000) {
       await interaction.reply({
-        content: "Maxigames doesn't have dice with that many sides :(",
+        content: "Your second name was too long! Try something shorter!",
         ephemeral: true,
       });
       return;
     }
-    if (dice < 1) {
-      await interaction.reply({
-        content: ":thinking: Maxigames won't do that.",
-        ephemeral: true,
-      });
-      return;
-    }
-    if (sides < 2) {
-      await interaction.reply({
-        content: "Dice can't have that many sides :(",
-        ephemeral: true,
-      });
-      return;
-    }
+    let calculated_length = Math.min(secondname.length, firstname.length);
+    let length_penalty =
+      calculated_length / Math.max(secondname.length, firstname.length);
     var dice_rolls = "";
-    for (var i = 0; i < dice; i++) {
-      const die_roll = Math.ceil(Math.random() * sides);
-      dice_rolls += die_roll;
-      dice_rolls += " ";
+    let percentage = length_penalty * 100;
+    for (let i = 0; i < calculated_length; i++) {
+      let char_of_1 = firstname[i];
+      let char_of_2 = secondname[i];
+      let asc1 = char_of_1.codePointAt(0) || 0;
+      let asc2 = char_of_2.codePointAt(0) || 0;
+      let complementation = (((asc1 + asc2) % 128) + 1) / 100;
+      percentage *= complementation;
     }
+    percentage = Math.min(100, Math.floor(percentage));
+
     const Embed = new Discord.MessageEmbed()
       .setColor("#00ff00")
-      .setTitle(`Dice roll results for ${dice} dice with ${sides} sides:`)
-      .setDescription(`${dice_rolls}`);
+      .setTitle(`How well do ${firstname} and ${secondname} ship?`)
+      .setDescription(`**${percentage}%**`);
     await interaction.reply({
       embeds: [Embed],
     });
