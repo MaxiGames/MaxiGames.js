efa5721a3752ad4ea0b51435a2ea635127a14467
neon_paradox <glennwy.sun@gmail.com>
2021-09-13 20:02:36 +0800

    ship is much more poggerz???

--- src/commands/general/ship.ts
+++ src/commands/general/ship.ts
@@ -23,12 +23,23 @@ const ship: MGCommand = {
           "Second object (if left blank, default set as your username)"
         )
         .setRequired(false)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("leniency")
+        .setDescription(
+          "Set as any value from 63 to 128. Using 63 results in higher leniency/ship results. [Default 80]"
+        )
+        .setRequired(false)
     ),
+
   async execute(interaction) {
     const firstname: string =
       interaction.options.getString("object") || "maxigames";
     const secondname: string =
       interaction.options.getString("object2") || interaction.user.username;
+    const leniency: number = interaction.options.getInteger("leniency") || 80;
+
     if (firstname.length > 1000) {
       await interaction.reply({
         content: "Your first name was too long! Try something shorter!",
@@ -43,9 +54,27 @@ const ship: MGCommand = {
       });
       return;
     }
+    if (leniency > 128) {
+      await interaction.reply({
+        content:
+          "Your leniency was too high. Set a value from 1 to 128 [80 default]!",
+        ephemeral: true,
+      });
+      return;
+    }
+    if (leniency < 63) {
+      await interaction.reply({
+        content:
+          "Your leniency was too low. Set a value from 1 to 128 [80 default]!",
+        ephemeral: true,
+      });
+      return;
+    }
     let calculated_length = Math.min(secondname.length, firstname.length);
-    let length_penalty =
-      calculated_length / Math.max(secondname.length, firstname.length);
+    let length_penalty = Math.sqrt(
+      calculated_length / Math.max(secondname.length, firstname.length)
+    );
+
     var dice_rolls = "";
     let percentage = length_penalty * 100;
     for (let i = 0; i < calculated_length; i++) {
@@ -53,17 +82,28 @@ const ship: MGCommand = {
       let char_of_2 = secondname[i];
       let asc1 = char_of_1.codePointAt(0) || 0;
       let asc2 = char_of_2.codePointAt(0) || 0;
-      let complementation = (((asc1 + asc2) % 128) + 1) / 100;
+      let complementation = (((asc1 + asc2) % 128) + 1) / leniency;
       percentage *= complementation;
     }
+    let actual_percentage = percentage;
+    actual_percentage = Math.floor(actual_percentage * 1000) / 1000;
     percentage = Math.min(100, Math.floor(percentage));
+    percentage = Math.max(1, percentage);
 
-    const Embed = new Discord.MessageEmbed()
+    const embed = MGEmbed(MGStatus.Info)
       .setColor("#00ff00")
       .setTitle(`How well do ${firstname} and ${secondname} ship?`)
-      .setDescription(`**${percentage}%**`);
+      .setDescription(`**${percentage}%**`)
+      .addFields(
+        {
+          name: "Actual Percentage :O",
+          value: actual_percentage + "%",
+          inline: false,
+        },
+        { name: "Leniency", value: leniency + " leniency", inline: false }
+      );
     await interaction.reply({
-      embeds: [Embed],
+      embeds: [embed],
     });
   },
 };
