9b15ccfacd842eba1faa4473795d553b967f02e1
AV3_08 <66512356+AV306@users.noreply.github.com>
2021-09-17 09:34:23 +0800

    :D

--- src/commands/cases/lispcase.ts
+++ src/commands/cases/lispcase.ts
@@ -1,3 +1,10 @@
+/*
+* File: src/commands/cases/lispcase.ts
+* Description: Handles convert to lispcase command
+*/
+
+
+
 import { SlashCommandBuilder } from "@discordjs/builders";
 import type MyCommand from "../../types/command";
 import { MGEmbed } from "../../lib/flavoured";
@@ -5,25 +12,30 @@ import MGStatus from "../../lib/statuses";
 
 let lispCaseFunc = require("lodash/kebabCase") as (param: string) => string;
 
+
+
+
 const lispCase: MyCommand = {
   data: new SlashCommandBuilder()
     .setName("lispcase")
     .setDescription("Convert some text into lispcase")
-    .addStringOption((option) =>
-      option
+    .addStringOption((option) => option
         .setName("string")
         .setDescription("Text that you want to change")
         .setRequired(true)
     ),
 
+	
   async execute(interaction) {
-    const str = interaction.options.getString("string");
-    if (str === null) return;
+    const toConvert = interaction.options.getString("string");
+		
+    if (toConvert === null) return; // no null string >:(
+		
     let embed = MGEmbed(MGStatus.Success)
       .setTitle("Succesfully lispcased!")
       .setDescription(`lisp-case Result: ${lispCaseFunc(str)}`);
 
-    await interaction.reply({ embeds: [embed] });
+    await interaction.reply({ embeds: [embed] }); // reply with embed defined above
   },
 };
 
