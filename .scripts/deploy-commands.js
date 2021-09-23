6f1f5dfb33825d0a7429862a2fe2db6721c9a81d
Isaac <tux314159@gmail.com>
2021-09-23 19:00:13 +0800

    Fixed command depoloy script...

--- .scripts/deploy-commands.js
+++ .scripts/deploy-commands.js
@@ -40,9 +40,12 @@ const rest = new REST({ version: "9" }).setToken(config.tokenId);
       });
     } else {
       console.log("Deploying commands locally onto Beta.");
-      await rest.put(Routes.applicationCommands(config.clientId), {
-        body: commands,
-      });
+      await rest.put(
+        Routes.applicationGuildCommands(config.clientId, config.guildId),
+        {
+          body: commands,
+        }
+      );
     }
 
     console.log("Successfully registered application commands.");
