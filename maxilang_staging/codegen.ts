f3201370f03813a754815b519d8081a998ba74d0
Isaac <tux314159@gmail.com>
2021-11-08 23:09:24 +0800

    Added auto dispatch generator to codegen

--- maxilang_staging/codegen.ts
+++ maxilang_staging/codegen.ts
@@ -14,10 +14,23 @@ const prelude =
  * 2. (lambda (<...>...) <...>)
  * 3. (if <...> <...> <...>)
  * 4. (let ((<...> <...>)...) <...>)
- * 5. (<and/or> <...> <...>)
- * 6. (begin <...>...)
- * 7. (; <...>)
+ * 5. (and <...> <...>)
+ * 6. (or <...> <...>)
+ * 7. (begin <...>...)
  */
+
+const special_forms = [
+	"define",
+	"lambda",
+	"if",
+	"let",
+	"and",
+	"or",
+	"begin",
+].map((x) => [x, eval("gen_js_" + x)]) as [
+	string,
+	(body: AST | AST[] | atom) => string
+][];
 
 // form: <atom>
 function gen_js_atom(body: atom): string {
@@ -30,7 +43,7 @@ function gen_js_atom(body: atom): string {
 
 // form: (define <name> <expr>)
 function gen_js_define(body: AST[]): string {
-	return `let ${(body[1] as atom).value}=${_gen_js(body[2])};`;
+	return `let ${(body[1] as atom).value}=${gen_js_dispatch(body[2])};`;
 }
 
 // form: (lambda (<param>...) <code>)
@@ -40,7 +53,7 @@ function gen_js_lambda(body: AST[]): string {
 	for (let pname of body[1] as atom[]) {
 		ret += pname.value + ",";
 	}
-	ret += `)=>{return ${_gen_js(body[2])}})`;
+	ret += `)=>{return ${gen_js_dispatch(body[2])}})`;
 
 	return ret;
 }
@@ -48,9 +61,9 @@ function gen_js_lambda(body: AST[]): string {
 // form: (if <cond> <expr-if-true> <expr-if-false>)
 function gen_js_if(body: AST[]) {
 	return (
-		`((()=>{if(${_gen_js(body[1])})` +
-		`{return ${_gen_js(body[2])}}` +
-		`else{return ${_gen_js(body[3])}}})())`
+		`((()=>{if(${gen_js_dispatch(body[1])})` +
+		`{return ${gen_js_dispatch(body[2])}}` +
+		`else{return ${gen_js_dispatch(body[3])}}})())`
 	);
 }
 
@@ -60,26 +73,39 @@ function gen_js_let(body: AST[]): string {
 
 	ret += "(((";
 	for (let pair of body[1] as AST[]) {
-		ret += `${(pair as [atom, AST])[0].value}=${_gen_js(
+		ret += `${(pair as [atom, AST])[0].value}=${gen_js_dispatch(
 			(pair as [atom, AST])[1]
 		)},`;
 	}
 	ret += ")=>{return(";
-	ret += _gen_js(body[2]);
+	ret += gen_js_dispatch(body[2]);
 	ret += ")})())";
 
 	return ret;
 }
 
-// form: (and <expr>...) OR (or <expr>...)
-function gen_js_andor(body: AST[]): string {
+// form: (and <expr>...)
+function gen_js_and(body: AST[]): string {
 	let ret = "";
 
 	ret += "(";
 	for (let node of body.slice(1)) {
-		ret += _gen_js(node) + (body[0] as atom).value === "and" ? "&&" : "||";
+		ret += gen_js_dispatch(node) + "&&";
 	}
-	ret += (body[0] as atom).value === "and" ? "true" : "false" + ")";
+	ret += "true)";
+
+	return ret;
+}
+//
+// form: (or <expr>...)
+function gen_js_or(body: AST[]): string {
+	let ret = "";
+
+	ret += "(";
+	for (let node of body.slice(1)) {
+		ret += gen_js_dispatch(node) + "||";
+	}
+	ret += "false)";
 
 	return ret;
 }
@@ -90,16 +116,11 @@ function gen_js_begin(body: AST[]): string {
 
 	ret += "((()=>{";
 	for (let node of body.slice(1, body.length - 1)) {
-		ret += _gen_js(node) + ";";
+		ret += gen_js_dispatch(node) + ";";
 	}
-	ret += `return ${_gen_js(body.slice(-1))};})())`;
+	ret += `return ${gen_js_dispatch(body.slice(-1))};})())`;
 
 	return ret;
-}
-
-// form: (; <comment>)
-function gen_js_comment(_: AST[]): string {
-	return "";
 }
 
 // form: (fn-name <arg-expr>...) OR <name>
@@ -111,59 +132,52 @@ function gen_js_call(body: AST): string {
 
 	let ret = "";
 	if (body[0] instanceof Array) {
-		ret += _gen_js(body[0]);
+		ret += gen_js_dispatch(body[0]);
 	} else {
 		ret += "(" + (body[0] as atom).value + ")";
 	}
 	ret += "(";
 	for (const node of body.slice(1)) {
-		ret += _gen_js(node) + ",";
+		ret += gen_js_dispatch(node) + ",";
 	}
 	ret += ")";
 
 	return ret;
 }
 
-function _gen_js(body: AST): string {
-	if (!(body instanceof Array)) {
-		return gen_js_atom(body);
-	}
+function gen_gen_js_dispatch(
+	specialforms: [string, (body: AST | AST[] | atom) => string][] // [name, rule]
+): (body: AST) => string {
+	function dispatch(body: AST): string {
+		if (!(body instanceof Array)) {
+			return gen_js_atom(body);
+		}
 
-	if (body[0] instanceof Array) {
-		return _gen_js(body[0]);
-	} else {
-		switch (body[0].type) {
-			case TokenType.string:
-			case TokenType.number:
-				return gen_js_atom(body[0]);
-			case TokenType.binding:
-				switch (body[0].value) {
-					case "define":
-						return gen_js_define(body);
-					case "lambda":
-						return gen_js_lambda(body);
-					case "if":
-						return gen_js_if(body);
-					case "let":
-						return gen_js_let(body);
-					case "and":
-					case "or":
-						return gen_js_andor(body);
-					case "begin":
-						return gen_js_begin(body);
-					case ";":
-						return gen_js_comment(body);
-					default:
-						return gen_js_call(body);
-				}
-			default:
-				throw new Error();
+		if (body[0] instanceof Array) {
+			return gen_js_dispatch(body[0]);
+		} else {
+			switch (body[0].type) {
+				case TokenType.string:
+				case TokenType.number:
+					return gen_js_atom(body[0]);
+				// deal with special forms here
+				case TokenType.binding:
+					return (specialforms.find(
+						(x) => x[0] === (body[0] as atom).value
+					) ?? [0, gen_js_call])[1](body);
+				default:
+					throw new Error();
+			}
 		}
 	}
+
+	return dispatch;
 }
 
+const gen_js_dispatch = gen_gen_js_dispatch(special_forms);
+
 function gen_js(body: AST): string {
-	return `(()=>{${prelude}${_gen_js(body)}})()`;
+	return `(()=>{${prelude}${gen_js_dispatch(body)}})()`;
 }
 
 console.log("\nCODE TO COMPUTE 20!:");
