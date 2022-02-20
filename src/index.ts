74bea8f416b8d798679ff787bc90270faeefb8f6
Ang Jun Ray <angjunray@live.com>
2022-02-20 19:07:12 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,6 @@ export const client = new Client({
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_MESSAGES,
   ],
   partials: ["MESSAGE", "REACTION"],
 });
