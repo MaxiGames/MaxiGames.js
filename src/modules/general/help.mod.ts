e7b41222b1e718a41f9438e99523874bd2e316f7
Ang Jun Ray <angjunray@live.com>
2022-03-19 15:26:52 +0800

    Fix Category not showing

--- src/modules/general/help.mod.ts
+++ src/modules/general/help.mod.ts
@@ -201,7 +201,7 @@ export async function mainHelp(
   return {
     embeds: [
       MGEmbed(MGStatus.Success)
-        .setTitle("MaxiGames Help! Category: **${startCase(page)}**")
+        .setTitle(`MaxiGames Help! Category: **${startCase(page)}**`)
         .setDescription(
           "To get support for specific queries and help, please join the MaxiGames" +
             "Official Server at https://discord.gg/hkkkTqhGAz and create a ticket."
